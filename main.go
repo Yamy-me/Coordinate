953a2c1176986652f70c4741d51bd41c5442c9a9
Atay <xaty041@gmail.com>
2026-03-29 21:41:21 +0600

    v2 Добавил gui и неявные функции, только работают плохо на графике

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"math"
+	"runtime"
+	"slices"
 	"strings"
 	"sync"
 
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"github.com/Knetic/govaluate"
 )
 
@@ -14,65 +21,73 @@ type Points struct {
 }
 
 func main() {
-	const height, width = 40, 40
-	CoordinateASCII := [height][width]string{}
-	points, err := coordinateFnc("(x ** 2 + y ** 2 - 1) ** 3 - x ** 2 * y ** 3")
-	if err != nil{
-		fmt.Println("error in main points section")
-		return
+	// Длина и ширина координатной плоскости
+	// const height, width = 10, 10
+	// CoordinateASCII := [height][width]string{}
+	functions := []string{"x ** 2 + y ** 2 - 25", "(x + 2) ** 2 + (y - 2) ** 2 - 0.5", "(x - 2) ** 2 + (y - 2) ** 2 - 0.5", "0.2 * x ** 2 - 3"}
+	
+	points := []Points{}
+	for _, v := range functions{
+		result, _ := coordinateFnc(v)
+		points = append(points, result...)
+		
 	}
-		for row := 0; row < height; row++ {
-			for col := 0; col < width; col++ {
-				CoordinateASCII[row][col] = " "
-			}
-		}
-		midRow := height / 2
-		midCol := width / 2
 
-		// горизонтальная ось
-		for col := 0; col < width; col++ {
-			CoordinateASCII[midRow][col] = "-"
-		}
+	app := app.New()
+	window := app.NewWindow("Название")
+	window.Resize(fyne.NewSize(1920, 1080))
 
-		// вертикальная ось
-		for row := 0; row < height; row++ {
-			CoordinateASCII[row][midCol] = "|"
-		}
+	// Горизонтальная линия
+	centerX := float32(1920 / 2)
+	vLine := canvas.NewLine(color.Black)
+	vLine.Position1 = fyne.NewPos(centerX, 0)
+	vLine.Position2 = fyne.NewPos(centerX, 1080)
 
-		// пересечение
-		CoordinateASCII[midRow][midCol] = "+"
-		xMax, xMin := points[0].X, points[0].X
-		yMax, yMin := points[0].Y, points[0].Y
+	// Вертикальная линия
+	centerY := float32(1080 / 2)
+	hLine := canvas.NewLine(color.Black)
+	hLine.Position1 = fyne.NewPos(0, centerY)
+	hLine.Position2 = fyne.NewPos(1920, centerY)
+	objectCanvas := []fyne.CanvasObject{hLine, vLine}
 
-		for _, v := range points {
-			if v.X > xMax {
-				xMax = v.X
-			}
-			if v.X < xMin {
-				xMin = v.X
-			}
-			if v.Y > yMax {
-				yMax = v.Y
-			}
-			if v.Y < yMin {
-				yMin = v.Y
-			}
-		}
-
+	// если функция не явная
+	if slices.ContainsFunc(functions, func(v string) bool {
+		return strings.Contains(v, "y")
+	}){
 		for _, p := range points {
-			col := int((p.X - xMin) / (xMax - xMin) * float64(width))
-			row := int((yMax - p.Y) / (yMax - yMin) * float64(height))
-			if row >= 0 && row < height && col >= 0 && col < width {
-				CoordinateASCII[row][col] = "*"
-			}
+		x := centerX + float32(p.X * 100)
+		y := centerY - float32(p.Y * 100)
+		dot := canvas.NewCircle(color.RGBA{255, 0, 0, 255})
+		dot.Resize(fyne.NewSize(1, 1))
+		dot.Move(fyne.NewPos(x, y))
+		objectCanvas = append(objectCanvas, dot)
 		}
-		for _, row := range CoordinateASCII {
-			fmt.Println(strings.Join(row[:], ""))
+
+	}else{
+		for i := 1; i < len(points); i++ {
+		x1 := centerX + float32(points[i-1].X * 100)
+		y1 := centerY - float32(points[i-1].Y * 100)
+
+		x2 := centerX + float32(points[i].X * 100)
+		y2 := centerY - float32(points[i].Y * 100)
+		
+		Line := canvas.NewLine(color.RGBA{255, 0, 0, 255})
+		Line.Position1 = fyne.NewPos(x1, y1)
+		Line.Position2 = fyne.NewPos(x2, y2)
+		objectCanvas = append(objectCanvas, Line)
+		
 		}
 	}
+	cont := container.NewWithoutLayout(objectCanvas...)
+	// добавление в контейнер
+	window.SetContent(cont)
+	window.ShowAndRun()
+	
+
+
+}
 
 func coordinateFnc(function string) ([]Points, error) {
-	var channel = make(chan Points)
 	var wg sync.WaitGroup
 	res := []Points{}
 	fncs := map[string]govaluate.ExpressionFunction{
@@ -97,15 +112,17 @@ func coordinateFnc(function string) ([]Points, error) {
 		return []Points{}, fmt.Errorf("Error in expr: NewEvaluableExpWithFnc: %w", err)
 	}
 	if strings.Contains(function, "y") {
-		for i := range 20 {
+		var channel = make(chan Points)
+		workers := runtime.NumCPU()
+		for i := range workers {
 			wg.Add(1)
 			go func(i int) {
 				defer wg.Done()
-				step := (10.0 - -10.0) / 10
+				step := (10.0 - -10.0) / float64(workers)
 				xStart := -10.0 + float64(i)*step
 				xEnd := -10.0 + float64((i+1))*step
-				for x := xStart; x <= xEnd; x += 0.05 {
-					for y := -10.0; y <= 10.0; y += 0.05{
+				for x := xStart; x <= xEnd; x += 0.005 {
+					for y := -10.0; y <= 10.0; y += 0.005 {
 						params := map[string]interface{}{
 							"x": x,
 							"y": y,
@@ -114,22 +131,22 @@ func coordinateFnc(function string) ([]Points, error) {
 						if err != nil {
 							continue
 						}
-						if math.Abs(result.(float64)) < 0.04 {
+						if math.Abs(result.(float64)) < 0.005 {
 							channel <- Points{X: x, Y: y}
 						}
 					}
-				}		
+				}
 			}(i)
 		}
 
-		go func ()  {
+		go func() {
 			wg.Wait()
 			close(channel)
 		}()
-		
+
 		for v := range channel {
 			res = append(res, Points{X: v.X, Y: v.Y})
-		
+
 		}
 
 	} else {
@@ -144,7 +161,6 @@ func coordinateFnc(function string) ([]Points, error) {
 			res = append(res, Points{X: x, Y: y.(float64)})
 		}
 	}
-
 
 	return res, nil
 }
